b5226d759f26c1148cb98db452b85ce35f97313a
rinfiyks <tom.gould.92@gmail.com>
2017-03-11 14:24:28 +0000

    Get random dave fact from the most recent 100 tweets.
    Requires a twitter_keys.py file with your OAuth details

--- Twitter.py
+++ Twitter.py
@@ -1,4 +1,17 @@
+import tweepy
+import twitter_keys
+
+from random import randint
+from tweepy import OAuthHandler
+
 class Twitter:
 
   def getDaveTweet(self):
-      print("Hello, world");
+    auth = OAuthHandler(twitter_keys.consumer_key, twitter_keys.consumer_secret)
+    auth.set_access_token(twitter_keys.access_token, twitter_keys.access_secret)
+     
+    api = tweepy.API(auth)
+
+    recent_tweets = api.user_timeline(user_id = '190974303', count = 100)
+    return recent_tweets[randint(0, 100)].text
+
