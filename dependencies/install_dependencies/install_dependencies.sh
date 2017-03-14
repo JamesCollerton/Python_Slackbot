e4b725d238debbe9bed277ee85c60795efd31d94
James Collerton <james.collerton@kobaltmusic.com>
2017-03-14 13:59:14 +0000

    Changed which neural network implementation we're using

--- dependencies/install_dependencies/install_dependencies.sh
+++ dependencies/install_dependencies/install_dependencies.sh
@@ -1,36 +1,8 @@
-# This is to act as a set of notes for how to install
-# the required dependencies for the project. The first
-# required ones are numpy, theano and filelock which can 
-# be installed using the below.
+# Need to download tensorflow for python to do calculations.
+# To do this we need Python 3.5.x found at the below link:
 
-# pip install numpy
-# pip install filelock
+# https://www.python.org/downloads/windows/
 
-# The second one that is needed is DeepPy. To install that
-# run the below:
+# Once downloaded need to install tensorflow using
 
-# git clone https://github.com/zomux/deepy
-# cd deepy
-# pip install -r requirements.txt
-
-# At this point you might end up with some problems installing 
-# scipy, so will need to use miniconda, which is found at the
-# below:
-
-# https://conda.io/miniconda.html
-
-# Download that and then run 
-
-# conda install scipy
-
-# Once scipy is installed you need to install theano using the
-# below
-
-# pip install theano
-
-# The final stage is to run the two below commands from inside 
-# deepy directory
-
-# source bin/cpu_env.sh
-# python experiments/mnist/mlp_dropout.py
-
+# pip3 install --upgrade tensorflow
