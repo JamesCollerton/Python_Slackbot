f6dadf1a767be81622888b2968c6b67060b8d237
James Collerton <james.collerton@kobaltmusic.com>
2017-03-17 09:28:11 +0000

    Need to run the neural network as a subprocess

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,10 @@
+import os
+
 class NeuralNetwork:
+
+# Command we're trying to emulate, might need to add network location:
+# Probably need to run this as a subprocess.
+# python neural_style.py --content ../images/dave_face.jpeg --styles examples/1-style.jpg --output examples/1-output.jpg --iterations 100
 
     def runNeuralNetwork(self):
 
