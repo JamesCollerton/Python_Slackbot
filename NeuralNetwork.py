8cab82556cf5f3ed3245d86493ab1f0fa3092619
James Collerton <james.collerton@kobaltmusic.com>
2017-03-21 13:38:31 +0000

    Think I have it moving an image from the images file into the neural network folder, now need to move out the output

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,11 +1,34 @@
 import os
+import shutil
 
 class NeuralNetwork:
 
-    def runNeuralNetwork(self):
+    bashString = 'python neural_style.py --content ../images/dave_face.jpeg --styles ../images/style.jpg --output ../images/output.jpg --iterations 10'
+
+    imageSourceLocation = 'images/';
+    imageDestLocation = 'dependencies/images/style.jpg';
+
+    def moveImage(self, imageName):
+        """
+        This is used to move the named image from the images
+        file into the temporary image folder and rename it to
+        style.jpg for the neural network to work on.
+        """
+        shutil.move(self.imageSourceLocation + imageName, self.imageDestLocation);
+
+    def executeNeuralNetwork(self):
         """
         Runs the neural network from the dependencies
         file.
         """
         os.chdir('dependencies/neural_artistic_style/');
-        os.system('bash runPy.sh');
+        os.system(self.bashString);
+
+    def runNeuralNetwork(self, imageName):
+        """
+        Moves an image from the input folder to the neural
+        networks folder, then runs the NN on it. Takes the
+        output and moves it to an output folder.
+        """
+        self.moveImage(imageName);
+        self.executeNeuralNetwork();
