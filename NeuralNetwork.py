ab9de9f43550f4c4f44d99e943bedd526ebc74c4
James Collerton <james.collerton@kobaltmusic.com>
2017-03-21 17:35:55 +0000

    Sorted out running network on image in folder, then moving it to output folder

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,16 +5,20 @@ class NeuralNetwork:
 
     bashString = 'python neural_style.py --content ../images/dave_face.jpeg --styles ../images/style.jpg --output ../images/output.jpg --iterations 10'
 
-    imageSourceLocation = 'images/';
-    imageDestLocation = 'dependencies/images/style.jpg';
+    imageSourceLocation = 'images_input/';
+    imageDestLocation = 'dependencies/images/';
+    imageDestFileName = 'style.jpg';
 
-    def moveImage(self, imageName):
+    imageOutputFileName = 'output.jpg'
+    imageSlackBotLocation = 'images_output/';
+
+    def moveImageIn(self, imageName):
         """
         This is used to move the named image from the images
         file into the temporary image folder and rename it to
         style.jpg for the neural network to work on.
         """
-        shutil.move(self.imageSourceLocation + imageName, self.imageDestLocation);
+        shutil.move(self.imageSourceLocation + imageName, self.imageDestLocation + self.imageDestFileName);
 
     def executeNeuralNetwork(self):
         """
@@ -23,6 +27,15 @@ class NeuralNetwork:
         """
         os.chdir('dependencies/neural_artistic_style/');
         os.system(self.bashString);
+        os.chdir('../../');
+
+    def moveImageOut(self):
+        """
+        This is used to move the image out from the neural
+        network folders into a top level folder to be used
+        by the SlackBot
+        """
+        shutil.move(self.imageDestLocation + self.imageOutputFileName, self.imageSlackBotLocation + self.imageOutputFileName);
 
     def runNeuralNetwork(self, imageName):
         """
@@ -30,5 +43,6 @@ class NeuralNetwork:
         networks folder, then runs the NN on it. Takes the
         output and moves it to an output folder.
         """
-        self.moveImage(imageName);
+        self.moveImageIn(imageName);
         self.executeNeuralNetwork();
+        self.moveImageOut();
