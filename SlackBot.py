26b9f908fef7a74ac04deb61e10155655fe74a02
James Collerton <james.collerton@kobaltmusic.com>
2017-04-04 13:38:51 +0100

    Got downloading a file working

--- SlackBot.py
+++ SlackBot.py
@@ -1,6 +1,8 @@
 import os
 import time
 import threading
+import requests
+import shutil
 
 import Keys
 from slackclient import SlackClient
@@ -16,15 +18,16 @@ class SlackBot:
     # Create thread for running SlackBot monitor
 
     def run(self):
-        """
-        Connects SlackBot using supplied credentials and
-        starts a thread looking for input every one second
-        """
-        if SlackBot.slack_client.rtm_connect():
-            print("StarterBot connected and running!")
-            threading.Timer(1, self.monitor).start()
-        else:
-            print("Connection failed. Invalid Slack token or bot ID?")
+        # """
+        # Connects SlackBot using supplied credentials and
+        # starts a thread looking for input every one second
+        # """
+        # if SlackBot.slack_client.rtm_connect():
+        #     print("StarterBot connected and running!")
+        #     threading.Timer(1, self.monitor).start()
+        # else:
+        #     print("Connection failed. Invalid Slack token or bot ID?")
+        self.getImage();
 
     def monitor(self):
         """
@@ -32,7 +35,6 @@ class SlackBot:
         slack channel and then passes it to the function which deals
         with it.
         """
-        print("Hello")
         command, channel = self.parse_slack_output(SlackBot.slack_client.rtm_read())
         if command and channel:
             self.handle_command(command, channel)
@@ -61,9 +63,36 @@ class SlackBot:
         output_list = slack_rtm_output
         if output_list and len(output_list) > 0:
             for output in output_list:
-                print(output)
                 if output and 'text' in output and SlackBot.AT_BOT in output['text']:
                     # return text after the @ mention, whitespace removed
                     return output['text'].split(SlackBot.AT_BOT)[1].strip().lower(), \
                            output['channel']
         return None, None
+
+    # def parse_slack_file_output(self, slack_rtm_output):
+    #     """
+    #     The Slack Real Time Messaging API is an events firehose.
+    #     this parsing function returns None unless a message is
+    #     directed at the Bot, based on its ID.
+    #     """
+    #     output_list = slack_rtm_output
+    #     if output_list and len(output_list) > 0:
+    #         for output in output_list:
+    #             if output and 'url_private' in output and SlackBot.AT_BOT in output['text']:
+    #                 # return text after the @ mention, whitespace removed
+    #                 return output['text'].split(SlackBot.AT_BOT)[1].strip().lower(), \
+    #                        output['channel']
+    #     return None, None
+
+    def getImage(self):
+        url = 'https://files.slack.com/files-pri/T4F8BB072-F4TGMV54J/donelli.jpg';
+        token = Keys.slack_bot_token;
+        path='test.jpg'
+        # requests.get(url, headers={'Authorization': 'Bearer %s' % token})
+
+        r = requests.get(url, stream=True, headers={'Authorization': 'Bearer %s' % token})
+        print(r.status_code)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
